cac3869fa6d99da5d451a9ed24fda076b925401e
tianjigezhu <tianjigezhuhn@aliyun.com>
2017-08-05 03:28:14 +0800

    第一次提交代码，是C++代码。 添加了两个文件，Paradise.hpp头文件用于定义命名空间。最高命名空间Paradise定义在这里，以后所有的C++代码需要定义的命名空间， 不仅仅是算法，都要在这个头文件里定义。SingleLinkList.hpp头文件定义单向链表算法。

---
+++ C++/Paradise.hpp
@@ -0,0 +1,21 @@
+//! 定义C+算法的命名空间
+/*
+ 每一个C++算法都放在命名空间Algorithm中。
+ 该命名空间又放在Paradise命名空间中。
+ @author tianjigezhu
+ @version 1.0
+ @data 2017/08/05 03:21:00
+*/
+
+#ifndef __MEND_HPP__
+#define __MEND_HPP__
+
+namespace Paradise
+{
+	namespace Algorithm
+	{
+
+	}
+}
+
+#endif
---
+++ C++/SingleLinkList.hpp
@@ -0,0 +1,3 @@
+#include "Paradise.hpp"
+
+namespace Paradise
