9145c3a2b2f828e8ba577209140c81fbbae48050
tianjigezhu <tianjigezhuhn@aliyun.com>
2017-08-06 01:58:05 +0800

    在Paradise.hpp中添加了注释，包括文件注释和命名空间注释。

--- C++/Paradise.hpp
+++ C++/Paradise.hpp
@@ -1,21 +1,62 @@
-//! 定义C+算法的命名空间
-/*
- 每一个C++算法都放在命名空间Algorithm中。
- 该命名空间又放在Paradise命名空间中。
- @author tianjigezhu
- @version 1.0
- @data 2017/08/05 03:21:00
-*/
+/*****************************************************************************
+*  C++ Algorithm tool library                                                *
+*  Copyright (C) 2017 Tianjigezhu tianjigezhuhn@aliyun.com                   *
+*                                                                            *
+*  This file is part of C++ Algorithm.                                       *
+*                                                                            *
+*  This program is free software; you can redistribute it and/or modify it   *
+*  under the terms of the GNU General Public License version 3 as published  *
+*  by the Free Software Foundation.                                          *
+*                                                                            *
+*  You should hava received a copy of the GNU General Public License along   *
+*  with C++ Algorithm. If not, see <http://www.gnu.org/licenses/>.           *
+*                                                                            *
+*  Unless required by applicable law or agreed to in writing, software       *
+*  distributed under the License is distributed on an "AS IS" BASIS,         *
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+*  See the License for the specific language governing permissions and       *
+*  limitations under the License.                                            *
+*                                                                            *
+*  @file     Paradise.hpp                                                    *
+*  @brief    Define the namespace Paradise and the namespaces in it.         *
+*  All my namespaces will be defined in it.                                  *
+*                                                                            *
+*  @author   Tianjigezhu                                                     *
+*  @email    tianjigezhuhn@aliyun.com                                        *
+*  @version  1.0.0.1                                                         *
+*  @date     2017/08/05                                                      *
+*  @license  GNU General Public License (GPL)                                *
+*                                                                            *
+*----------------------------------------------------------------------------*
+*  Remark         : Description                                              *
+*----------------------------------------------------------------------------*
+*  Change History :                                                          *
+*  <Date>     | <Version> | <Author>       | <Description>                   *
+*----------------------------------------------------------------------------*
+*  2017/08/05 | 1.0.0.1   | Tianjigezhu    | Create file                     *
+*----------------------------------------------------------------------------*
+*                                                                            *
+*****************************************************************************/
 
-#ifndef __MEND_HPP__
-#define __MEND_HPP__
+#ifndef __PARADISE_HPP__
+#define __PARADISE_HPP__
 
+/**
+ * @brief 定义了命名空间Paradise，包含其他命名空间
+ * 命名空间Paradise是所有项目的最高命名空间，其他项目的命名空间都必须定义在这命名空间了内。
+ */
 namespace Paradise
 {
+
+	/**
+	 * @brief 定义了命名空间Algorithm
+	 * 命名空间Algorithm是C++算法的命名空间，所有C++数据结构与算法都定义在此命名空间内。
+	 */
 	namespace Algorithm
 	{
 
-	}
-}
+	} // namespace Algorithm
 
-#endif
+} // namespace Paradise
+
+#endif // __PARADISE_HPP__
